1b39a51f46f17678781b7837f8ab6cfe52c40af7
0xb10c <b10c@b10c.me>
2023-10-09 18:07:52 +0200

    wip: slow gzip loading
    this is really slow. 20s for a full new table...

--- www/js/main.js
+++ www/js/main.js
@@ -48,22 +48,76 @@ document.getElementById("load-url").onclick = function () {
 
 document.getElementById('selectFiles').addEventListener('change', function(e) {
   if (e.target.files[0]) {
-    var fr = new FileReader();
-    fr.onload = function (e) {
-      let getrawaddrman = JSON.parse(e.target.result);
-      processGetRawAddrman(getrawaddrman);
+    let fr = new FileReader();
+    let type;
+    fr.onload = async function (e) {
+      if (type == "json") {
+        let getrawaddrman = JSON.parse(e.target.result);
+        processGetRawAddrman(getrawaddrman);
+      } else if (type == "gzip") {
+        
+      }
     };
     var files = document.getElementById("selectFiles").files;
+    let file = files.item(0)
 
-    fr.readAsText(files.item(0));
+    if (file.type == "application/json") {
+      type = "json";
+      fr.readAsText(file);
+    } else if (file.type == "application/gzip") {
+      type = "gzip";
+      readAndDecompressGzip(file);
+      //fr.readAsArrayBuffer(file)
+    } else {
+      console.error("Unsupported file type ", file.type)
+    }
   }
 });
 
 window.onload = (event) => {
-
   const searchParams = new URLSearchParams(window.location.search);
   if (searchParams.has("url")) {
     loadFromURL(searchParams.get("url"));
   }
-
 };
+
+async function readAndDecompressGzip(file) {
+  const arrayBuffer = await file.arrayBuffer();
+  const gzipData = new Uint8Array(arrayBuffer);
+
+  try {
+    const decompressedText = await decompressGzip(gzipData);
+    console.log("drawing")
+    processGetRawAddrman(JSON.parse(decompressedText));
+  } catch (error) {
+    console.error('Error decompressing GZIP file:', error);
+  }
+}
+
+async function decompressGzip(data) {
+  const decompressionStream = new DecompressionStream('gzip');
+  const readableStream = new ReadableStream({
+    start(controller) {
+      controller.enqueue(data);
+      controller.close();
+    }
+  });
+
+  const decompressedStream = readableStream.pipeThrough(decompressionStream);
+  const chunks = [];
+  const reader = decompressedStream.getReader();
+
+  try {
+    while (true) {
+      const { done, value } = await reader.read();
+      if (done) break;
+      chunks.push(value);
+    }
+
+    const concatenatedChunks = new Uint8Array([...chunks.reduce((acc, chunk) => [...acc, ...Array.from(chunk)], [])]);
+    const textDecoder = new TextDecoder();
+    return textDecoder.decode(concatenatedChunks);
+  } finally {
+    reader.releaseLock();
+  }
+}
